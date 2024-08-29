5eb2fb3101d2676dea7fa383cfb41087e2448dbf
DamyanBG <d.dimitrov.1988@abv.bg>
2024-08-29 15:35:40 +0300

    Simple ES CRUD operations

---
+++ src/main.rs
@@ -0,0 +1,196 @@
+use elasticsearch::{ http::{ response::Response, transport::Transport }, indices::{ IndicesCreateParts, IndicesExistsParts }, Elasticsearch, IndexParts, SearchParts };
+use std::error::Error;
+use serde_json::{ json, Value };
+use serde::{ Deserialize, Serialize };
+
+struct ElSearch {
+    client: Elasticsearch,
+}
+
+impl ElSearch {
+    fn new_from_localhost(host: &str) -> Self {
+        let transport = Transport::single_node(host).unwrap();
+        let es_client = Elasticsearch::new(transport);
+        ElSearch {
+            client: es_client
+        }
+    }
+
+    async fn search(&self, index_name: &str, body: &Value) -> Result<Response, Box<dyn Error>> {
+        let response = self.client
+            .search(SearchParts::Index(&[index_name]))
+            .body(body)
+            .send()
+            .await?;
+        Ok(response)
+    }
+
+    async fn add_document(&self, index_name: &str, body: &Value) -> Result<Response, Box<dyn Error>> {
+        let response = self.client
+            .index(IndexParts::Index(index_name))
+            .body(body)
+            .send()
+            .await?;
+        Ok(response)
+    }
+
+    async fn check_index_exists(&self, index_name: &str) -> Result<Response, Box<dyn Error>> {
+        let response = self.client
+            .indices()
+            .exists(IndicesExistsParts::Index(&[index_name]))
+            .send()
+            .await?;
+        Ok(response)
+    }
+
+    async fn create_index(&self, index_name: &str, body: &Value) -> Result<Response, Box<dyn Error>> {
+        let response: Response = self.client
+            .indices()
+            .create(IndicesCreateParts::Index(index_name))
+            .body(body)
+            .send()
+            .await?;
+        Ok(response)
+    }
+    
+}
+ 
+fn get_product_mapping() -> Value {
+    json!({
+        "mappings": {
+            "properties": {
+                "name": {
+                    "type": "text",
+                    "analyzer": "standard"
+                },
+                "description": {
+                    "type": "text",
+                    "analyzer": "standard"
+                },
+                "category": {
+                    "type": "keyword"
+                },
+                "brand": {
+                    "type": "keyword"
+                },
+                "price": {
+                    "type": "float"
+                },
+                "rating": {
+                    "type": "float"
+                }
+            }
+        }
+    })
+}
+
+fn create_client() -> Result<Elasticsearch, Box<dyn Error>> {
+    let transport = Transport::single_node("http://localhost:9200")?;
+    Ok(Elasticsearch::new(transport))
+}
+
+async fn search(client: &Elasticsearch, index_name: &str, body: &Value) -> Result<Response, Box<dyn Error>> {
+    let response = client
+        .search(SearchParts::Index(&[index_name]))
+        .body(body)
+        .send()
+        .await?;
+    Ok(response)
+}
+
+async fn create_index(index_name: &str, client: &Elasticsearch, body: &Value) -> Result<Response, Box<dyn Error>> {
+    let response: Response = client
+        .indices()
+        .create(IndicesCreateParts::Index(index_name))
+        .body(body)
+        .send()
+        .await?;
+    Ok(response)
+}
+
+async fn check_index_exists(index_name: &str, client: &Elasticsearch) -> Result<Response, Box<dyn Error>> {
+    let response = client
+        .indices()
+        .exists(IndicesExistsParts::Index(&[index_name]))
+        .send()
+        .await?;
+    Ok(response)
+}
+
+async fn add_document(client: &Elasticsearch, index_name: &str, body: &Value) -> Result<Response, Box<dyn Error>> {
+    let response = client
+        .index(IndexParts::Index(index_name))
+        .body(body)
+        .send()
+        .await?;
+    Ok(response)
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Product {
+    brand: String,
+    category: String,
+    description: String,
+    name: String,
+    price: f64,
+    rating: f64,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let product_index_name = "products";
+
+    let es = ElSearch::new_from_localhost("http://localhost:9200");
+    
+    let is_existing_resp = es.check_index_exists(product_index_name).await?;
+    let index_exists = is_existing_resp.status_code().is_success();
+
+    if index_exists {
+        println!("Index {} exists!", product_index_name);
+    } else {
+        println!("Index {} does not exists! Proceed with creating", product_index_name);
+        let product_mapping = get_product_mapping();
+        let create_resp = es.create_index(product_index_name,  &product_mapping).await?;
+
+        if create_resp.status_code().is_success() {
+            println!("Index created successfully.");
+        } else {
+            println!("Failed to create index: {:?}", create_resp.text().await?);
+        }
+    }
+
+    let product_doc = json!({
+        "name": "Eco-Friendly Bamboo Toothbrush",
+        "description": "A sustainable toothbrush with a bamboo handle and biodegradable bristles, designed for environmentally conscious users.",
+        "category": "Eco-Friendly Products",
+        "brand": "GreenBrush",
+        "price": 12.99,
+        "rating": 4.6
+    });
+
+    let add_resp: Response = es.add_document(product_index_name, &product_doc).await?;
+
+    println!("{:?}", add_resp);
+
+    let query = json!(
+        {
+            "query": {
+                "multi_match": {
+                "query": "toothbrush",
+                "fields": ["name", "description"]
+                }
+            }
+        }
+    );
+
+    let search_resp = es.search(product_index_name, &query).await?;
+
+    let resp_body = search_resp.json::<Value>().await?;
+
+    for hit in resp_body["hits"]["hits"].as_array().unwrap() {
+        let product: Product = serde_json::from_value(hit["_source"].clone())?;
+        println!("{:?}", product);
+    }
+
+    Ok(())
+}
